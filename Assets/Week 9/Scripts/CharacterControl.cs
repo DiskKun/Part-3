8008876ebd403634f8c25f0c2c53c1df5c481284
Lucas Hourahine <luca5h1905@gmail.com>
2024-03-14 11:43:13 -0400

    Task 4b complete: text UI element reflects selected character

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI typeText;
+
+    public static CharacterControl instance;
+
+    private void Start()
+    {
+        instance = this;
+        instance.typeText.GetComponent<TextMeshProUGUI>().text = "None Selected";
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
+        
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        instance.typeText.GetComponent<TextMeshProUGUI>().text = SelectedVillager.CanOpen().ToString();
+            
     }
     
 }
