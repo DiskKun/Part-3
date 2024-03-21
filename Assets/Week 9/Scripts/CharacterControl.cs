58647c736fa82099c32fcb9f1690a3d81b5cfa5f
Lucas Hourahine <luca5h1905@gmail.com>
2024-03-21 12:31:20 -0400

    Task 4a and 4b complete: selected villager changes with dropdown, villager has world space canvas and text ui element, slider changes scale of selected villager.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -6,16 +6,27 @@ using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
-    public TextMeshProUGUI typeText;
+    //public TextMeshProUGUI typeText;
 
     public static CharacterControl instance;
+
+    public List<Villager> vs = new List<Villager>();
 
     private void Start()
     {
         instance = this;
-        instance.typeText.GetComponent<TextMeshProUGUI>().text = "None Selected";
+        //instance.typeText.GetComponent<TextMeshProUGUI>().text = "None Selected";
     }
 
+    public void onDropDownChange(Int32 value)
+    {
+        SetSelectedVillager(vs[value]);
+    }
+
+    public void OnSliderChange(Single value)
+    {
+        SelectedVillager.transform.localScale = Vector3.one * value;
+    }
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -26,7 +37,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
-        instance.typeText.GetComponent<TextMeshProUGUI>().text = villager.ToString();
+        //instance.typeText.GetComponent<TextMeshProUGUI>().text = villager.ToString();
             
     }
     
--- Assets/Week 9/Scripts/Villager.cs
+++ Assets/Week 9/Scripts/Villager.cs
@@ -29,11 +29,11 @@ public class Villager : MonoBehaviour
         highlight.SetActive(isSelected);
     }
 
-    private void OnMouseDown()
-    {
-        CharacterControl.SetSelectedVillager(this);
-        clickingOnSelf = true;
-    }
+    //private void OnMouseDown()
+    //{
+    //    CharacterControl.SetSelectedVillager(this);
+    //    clickingOnSelf = true;
+    //}
 
     private void OnMouseUp()
     {
@@ -47,11 +47,11 @@ public class Villager : MonoBehaviour
         //flip the x direction of the game object & children to face the direction we're walking
         if(movement.x > 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            //transform.localScale = new Vector3(-1, 1, 1);
         }
         else if (movement.x < 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            //transform.localScale = new Vector3(1, 1, 1);
         }
 
         //stop moving if we're close enough to the target
